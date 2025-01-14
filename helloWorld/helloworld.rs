7cd1d2cb446e922089f78e5dcc98dc91ea6ceca4
andgamespace <anshxc@gmail.com>
2025-01-13 18:30:05 -0600

    .

---
+++ helloWorld/helloworld.rs
@@ -0,0 +1,7 @@
+// Comments in rust are the same as in any other language
+// just two slashes will do 
+fn main() {
+    // Statements here are exectuted when compiled binary is callled.
+    // Print text to the console
+    println!("Hello World!");
+}
